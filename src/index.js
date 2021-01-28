063e00ae6efbd187f30cac1daff19b3734159060
Franck Kabasele <kabasele467@gmail.com>
2021-01-28 17:48:09 -0500

    Integration Socket

--- src/index.js
+++ src/index.js
@@ -11,9 +11,17 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+let count = 0;
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New connection')
+
+    socket.emit('countUpdated', count)
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
