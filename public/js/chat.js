8209e031a36ef1a2acf644714efb534326be9346
Franck Kabasele <kabasele467@gmail.com>
2021-01-28 19:29:14 -0500

    share location with Geolocation Api

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,18 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     socket.emit('sendMessage', message)
 })
 
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('Geolocation is not support by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        socket.emit('sendPosition', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
+
--- src/index.js
+++ src/index.js
@@ -12,12 +12,26 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-
+// io is socket wrapping the server
 io.on('connection', (socket) => {
+    //On a new connection console.log new connection
     console.log('New connection')
+    // on a new connection send a welcome message
     socket.emit('message', 'Welcome!')
+    // on a new connection boracast a new user connection
+    socket.broadcast.emit('message', 'A new user has joined')
+    //listen to sendMessage and emit or send it to every user with io.emit
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    //listen to sendPositon and send it to every user
+    socket.on('sendPosition', (position) => {
+        io.emit('message', `https://google.com/maps?q=${position.latitude},${position.longitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
     })
 
   
