64dc621e7cbed49f122971eb7e04a4d9fb402b68
Franck Kabasele <kabasele467@gmail.com>
2021-01-29 21:17:25 -0500

    render message with mustache template

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,36 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationSend = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)   
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message, (error) => {
+
+        $messageFormButton.removeAttribute('disabled'),
+        $messageFormInput.value = " ",
+        $messageFormInput.focus()
+
         if(error){
             return console.log(error)
         }
@@ -18,10 +39,13 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 })
 
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationSend.addEventListener('click', () => {
+
     if(!navigator.geolocation){
         return alert('Geolocation is not support by your browser')
     }
+
+    $locationSend.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         console.log(position)
@@ -29,6 +53,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $locationSend.removeAttribute('disabled')
             console.log('Location Shared')
         })
     })
